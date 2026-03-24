32e3a01481c65007e947078584d3760123d578ab
Zakelly <zakelly.lan@gmail.com>
2026-03-24 21:27:54 +0800

    feat(cli): entry point for web-monitor

--- cobble-cli/src/main.rs
+++ cobble-cli/src/main.rs
@@ -1,4 +1,5 @@
 use cobble::{Config, RemoteCompactionServer};
+use cobble_web_monitor::{MonitorConfig, MonitorConfigSource, MonitorServer};
 use log::LevelFilter::Info;
 use std::error::Error;
 
@@ -21,6 +22,7 @@ fn run() -> Result<(), Box<dyn Error>> {
     }
     match command.as_str() {
         "remote-compactor" => run_remote_compactor(args),
+        "web-monitor" => run_web_monitor(args),
         _ => {
             print_usage();
             Err(format!("Unknown command: {}", command).into())
@@ -67,6 +69,49 @@ fn run_remote_compactor(mut args: impl Iterator<Item = String>) -> Result<(), Bo
     Ok(())
 }
 
+fn run_web_monitor(mut args: impl Iterator<Item = String>) -> Result<(), Box<dyn Error>> {
+    let mut config_path: Option<String> = None;
+    let mut bind_addr: Option<String> = None;
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "--config" => {
+                config_path = args.next();
+            }
+            "--bind" | "--address" => {
+                bind_addr = args.next();
+            }
+            "--help" | "-h" => {
+                print_usage();
+                return Ok(());
+            }
+            _ => {
+                return Err(format!("Unknown argument: {}", arg).into());
+            }
+        }
+    }
+
+    let Some(config_path) = config_path else {
+        return Err("web-monitor requires --config <path>".into());
+    };
+
+    let mut server = MonitorServer::new(MonitorConfig {
+        source: MonitorConfigSource::ConfigPath(config_path),
+        bind_addr: bind_addr.unwrap_or_else(|| "127.0.0.1:0".to_string()),
+        global_snapshot_id: None,
+        ..MonitorConfig::default()
+    })?;
+
+    let handle = server.serve()?;
+    eprintln!("web-monitor listening on http://{}/", handle.bind_addr());
+
+    // Keep process alive until interrupted; axum server runs on monitor runtime.
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(3600));
+    }
+}
+
 fn print_usage() {
-    eprintln!("Usage:\n  cobble-cli remote-compactor [--config <path>] [--bind <host:port>]\n");
+    eprintln!(
+        "Usage:\n  cobble-cli remote-compactor [--config <path>] [--bind <host:port>]\n  cobble-cli web-monitor --config <path> [--bind <host:port>]\n"
+    );
 }
